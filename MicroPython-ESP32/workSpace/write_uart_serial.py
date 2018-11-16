218a32e85a4e45178c9b061621d91039657ed029
lbandeira <lbms@cin.ufpe.br>
2018-11-15 21:42:50 -0300

    Plano B: Comunicação BT com um módulo externo

---
+++ MicroPython-ESP32/workSpace/write_uart_serial.py
@@ -0,0 +1,28 @@
+#****************************************************************************************************************************
+#   --------------------------------------------------------------------------------------------------------------------
+#   @Autecla: Send data to APP
+#   Sketch/program to send data from Control_Module to APP using an external Bluetooth connected on UART(2)
+#   Tip: To test this program, you can use Bluetooth terminal on your smartphone to connect to HC-06
+#
+#   ESP32 <--> BT HC-06
+#   Ports:
+#      Pin_RX = TX2
+#      Pin_TX = RX2
+#   --------------------------------------------------------------------------------------------------------------------
+#****************************************************************************************************************************
+
+
+
+from machine import UART
+import time
+
+urt = UART(2, 9600)
+urt.init(9600, bits=8, parity=None, stop=1, tx=17, rx=16)
+while True:
+    urt.write('o')
+    time.sleep(1)
+
+    #This part is to receive data from smartphone
+    if (urt.any()):
+      dados = urt.readline()
+      print('Dado:', dados)
